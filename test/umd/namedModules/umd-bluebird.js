596da6d0c1af84458d256f06c20ae4d8c4e5a5a6
Marius Gundersen <me@mariusgundersen.net>
2016-09-26 21:53:01 +0200

    Added failing test case for issue #5

---
+++ test/umd/namedModules/umd-bluebird.js
@@ -0,0 +1,9 @@
+function test(e){
+  if("object"==typeof exports&&"undefined"!=typeof module)
+    module.exports=e();
+  else if("function"==typeof define&&define.amd)
+    define('umd-bluebird', [],e);
+  else{
+    window.Promise=e()
+  }
+}
